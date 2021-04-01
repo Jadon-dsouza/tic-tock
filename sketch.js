db991c044d6a9b40be8b8b96bdd9fd039ba4721b
Jadon-dsouza <74820224+Jadon-dsouza@users.noreply.github.com>
2021-04-01 09:46:11 +0100

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,12 @@
+var hour;
+var minute;
+var second;
+function setup() {
+  createCanvas(800,400);
+  createSprite(400, 200, 50, 50);
+}
+
+function draw() {
+  background(255,255,255);  
+  drawSprites();
+}
